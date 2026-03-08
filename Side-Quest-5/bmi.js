0eada02ef1ab939a5ca0a3894898b8f42e199a8a
Jj01001010 <jerichojj08@gmail.com>
2026-03-08 23:23:19 +0800

    zzz

---
+++ Side-Quest-5/bmi.js
@@ -0,0 +1,34 @@
+const height = document.querySelector(".height")
+const weight = document.querySelector(".weight")
+const myForm = document.querySelector(".myForm")
+const resultEl = document.querySelector(".result-el")
+
+myForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    calculateBMI()
+})
+
+function calculateBMI() {
+
+    const heightValue = Number(height.value)
+    const weightValue = Number(weight.value)
+
+    if (isNaN(heightValue) || isNaN(weightValue) || heightValue <= 0 || weightValue <= 0) {
+        resultEl.textContent = "Invalid values"
+        return;
+    }
+
+    const heightInMeters = heightValue / 100
+    const bmi = weightValue / (heightInMeters * heightInMeters)
+
+    if (bmi <= 18.5) {
+        resultEl.textContent = `BMI: ${bmi.toFixed(2)} - Underweight`
+    } else if (bmi <= 24.9) {
+        resultEl.textContent = `BMI: ${bmi.toFixed(2)} - Normal weight`
+    } else if (bmi <= 29.9) {
+        resultEl.textContent = `BMI: ${bmi.toFixed(2)} - Overweight`
+    } else resultEl.textContent = `BMI: ${bmi.toFixed(2)} - Obese`
+
+    height.value = ""
+    weight.value = ""
+}
